a1a816181f9e0b0bffc74083b61c7d26b8e49002
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-01-25 10:26:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let arr=[];
+
+async function image_meal() {
+    const input = document.getElementById("searchbox").value;
+   // console.log(input);
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
+    const res = await data.json()
+    //console.log(res); 
+    await(res.meals).map((ele) =>{
+        arr.push(ele);
+        // const img = document.createElement("img")
+        // img.src=`${ele.strMealThumb}`;
+        // img.alt="meals" ;
+        // data_1.innerHTML=img;
+        // data.appendChild(img);
+        // console.log(data)
+        //console.log(ele.strMealThumb);
+    })
+    //console.log(res);
+    arr.map((ele)=>{
+        const img = document.createElement("img")
+        img.src=`${ele.strMealThumb}`;
+        let data_1 = document.getElementById("show_data");
+        data_1.appendChild(img);
+    })
+}
+
+
+
